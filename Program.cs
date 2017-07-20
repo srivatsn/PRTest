29138beb0ca89608c60eeb7e5dd6d33d9004bf47
Srivatsn Narayanan <srivatsn@microsoft.com>
2017-07-20 15:53:33 -0700

    More changes

--- Program.cs
+++ Program.cs
@@ -2,12 +2,16 @@
 
 namespace PRTest
 {
-    class Program
-    {
+    class Program {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
         }
+    }
+
+    class Foo
+    {
+        public async void SomeMethod() { }
     }
 }
